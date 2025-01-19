87918e94276834f03d82f7d5805b007711f8d4ca
Yordanos Solomon <yordanos.sole@gmail.com>
2025-01-19 14:25:06 +0300

    The function that handles .active style removed and no longer needed here

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,44 +1,24 @@
 import Header from "./components/Header";
-import About from "./components/About";
-import Education from "./components/Education";
-import ProjectCard from "./components/ProjectCard";
-import Testimonial from "./components/Testimonial";
+import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  let sections = document.querySelectorAll("section");
-  let navLinks = document.querySelectorAll("header nav a");
-  
-  window.onscroll = () => {
-    sections.forEach((sec) => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 150;
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute("id");
-  
-      if (top >= offset && top < offset + height) {
-        navLinks.forEach((links) => {
-          links.classList.remove("active");
-          document
-            .querySelector("header nav a [href*=" + id + " ]")
-            .classList.add("active");
-        });
-      }
-    });
-  };
-  
   return (
     <>
-      <Header></Header>
-      <About></About>
-      <Education></Education>
-      <ProjectCard></ProjectCard>
-      <Testimonial></Testimonial>
-      <Contact></Contact>
-      <Footer></Footer>
-
+      <Router>
+        <div>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer></Footer>
+        </div>
+      </Router>
     </>
   );
 }
