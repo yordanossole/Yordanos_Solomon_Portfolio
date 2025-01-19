7245335eefaeb5c91f6934eb3712bbe7d1f818cd
Yordanos Solomon <yordanos.sole@gmail.com>
2025-01-19 14:23:01 +0300

    The function that handles .active style removed and no longer needed here

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import About from "./About";
+import Education from "./Education";
+import Testimonial from "./Testimonial";
+import Skill from './Skill'
+
+function Home() {
+  return (
+    <>
+      <About></About>
+      <Education></Education>
+      <Skill></Skill>
+      <Testimonial></Testimonial>
+    </>
+  );
+}
+
+export default Home;
